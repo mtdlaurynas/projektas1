2b9a071d89748dd73d872ba987715fa4aa85c253
unknown <deividasnau12@gmail.com>
2018-02-07 20:45:27 +0200

    #

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<title>DigiByteOnline.org - Decentralized Exchange Fundraising</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/css/bootstrap.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Lato", sans-serif;
+  }
+
+  body,
+  html {
+    height: 100%;
+  }
+</style>
+
+<body>
+
+  <!-- Navigacijos meniu -->
+  <nav class="navbar">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <a class="btn btn-primary" href="#">Sign In</a>
+    </div>
+  </nav>
+  <footer>2018 &copy; DigiByteOnline.org</footer>
+  <script src="/js/bootstrap.js"></script>
+</body>
+
+</html>
