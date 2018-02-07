648f7bb4d1db95a019002f7bb3acc5e561a26643
stiklius <mtdlaurynas@gmail.com>
2018-02-07 21:01:49 +0200

    Idejau pagrinda ir dar CSS

--- index.php
+++ index.php
@@ -36,3 +36,66 @@
   <footer>2018 &copy; DigiByteOnline.org</footer>
   <script src="/js/bootstrap.js"></script>
 </body>
+
+<!DOCTYPE html>
+<html>
+<title>DigiByteOnline.org - Decentralized Exchange Fundraising</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/css/bootstrap.css">
+<link rel="stylesheet" href="/css/style.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Lato", sans-serif;
+  }
+
+  body,
+  html {
+    height: 100%;
+  }
+</style>
+
+<body>
+
+  <!-- Navigacijos meniu -->
+  <nav class="navbar">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <a class="btn btn-primary" href="#">Sign In</a>
+    </div>
+  </nav>
+  <footer>2018 &copy; DigiByteOnline.org</footer>
+  <script src="/js/bootstrap.js"></script>
+</body>
+
+<!-- Pagrindas -->
+<header class="masthead text-white text-center">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-9 mx-auto">
+        <h1 class="mb-5">Kazkoks random tekstas kurio niekaip negaliu sugalvot :D!</h1>
+      </div>
+      <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+        <form>
+          <div class="form-row">
+            <div class="col-12 col-md-9 mb-2 mb-md-0">
+              <input type="email" class="form-control form-control-lg" placeholder="kazkoks info...">
+            </div>
+            <div class="col-12 col-md-3">
+              <button type="submit" class="btn btn-block btn-lg btn-primary">Uzsiregink!</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</header>
