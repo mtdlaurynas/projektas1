eba9547ff4116cc5170dd53c716b1b85e14ad073
stiklius <mtdlaurynas@gmail.com>
2018-02-07 20:17:26 +0200

    Idetas navigacijos meniu

--- index.php
+++ index.php
@@ -7,13 +7,33 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif;}
-body, html {
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Lato", sans-serif;
+  }
+
+  body,
+  html {
     height: 100%;
-}
+  }
 </style>
+
 <body>
-<footer>2018 &copy; DigiByteOnline.org</footer>
-<script src="/js/bootstrap.js"></script>
+
+  <!-- Navigacijos meniu -->
+  <nav class="navbar navbar-light bg-light static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <a class="btn btn-primary" href="#">Sign In</a>
+    </div>
+  </nav>
+  <footer>2018 &copy; DigiByteOnline.org</footer>
+  <script src="/js/bootstrap.js"></script>
 </body>
+
 </html>
